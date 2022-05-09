0617dba0784025e5464cb7e25720f32469ba15d2
reine06 <104780983+reine06@users.noreply.github.com>
2022-05-09 20:35:58 +0100

    Add files via upload

---
+++ week2/day1/exerciceXP/ExercicesXP.js
@@ -0,0 +1,14 @@
+//Exercice 1
+
+		/*let nourriture_prefere = 'pommes sautés'
+		let petit_dejeune = 'BHB'
+		let diner = 'poisson braise'
+		let repas_prefere = 'I eat'  + nourriture_prefere + ' ,at every ' + diner*/
+
+
+//exercice 2 parti 1
+
+let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+
+
+let myWatchedSeries = 'I watched 3 series : black mirror, money heist, and the big bang theory'

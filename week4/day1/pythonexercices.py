d61084f5c6b35b3f377b7d38d7ed8b97cbe0b45c
reine06 <104780983+reine06@users.noreply.github.com>
2022-05-23 20:39:30 +0100

    Update pythonexercices.py

--- week4/day1/pythonexercices.py
+++ week4/day1/pythonexercices.py
@@ -23,3 +23,32 @@ shoe_size = 45
 info = f"je suis {name} j\'ai {age} ans et ma pointure de chaussure c'est {shoe_size}"
 '''print("je suis {} j'ai {}ans  et ma pointure de chaussure c'est {}".format(name,age,shoe_size))'''
 print(info)
+
+'''exercice 6 A ET B'''
+'''a = 15
+b = 8
+if a > b :
+    print("Hello world")'''
+
+'''exercice 7 pair ou impaire'''
+'''x = int(input("entrer un nombre svp"))
+if x % 2 == 0 :
+    print('pair')
+elif x % 2 !=0 :
+    print('impair')'''
+
+'''exercice 8 comment tu t'appele'''
+'''y = input("entrez votre nom svp:\n")
+myname= "reine"
+if y != myname :
+    print("dommage on a pas le meme nom")
+elif y == myname :
+    print("wouah quel coincidence")'''
+
+'''exercice 9 assez grand'''
+taille = int(input("quel est votre taille\n"))
+t = 145
+if taille > t :
+    print("vous etes assez grands")
+elif taille < t :
+    print("vous devez encore grandit")
